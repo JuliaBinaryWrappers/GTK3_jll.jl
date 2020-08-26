93dac42b7362ed4c5ae8643fa81ae4aef8d4d668
jlbuild <juliabuildbot@gmail.com>
2020-08-26 18:32:49 +0000

    GTK3_jll build 3.24.11+3

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -29,6 +29,7 @@ using at_spi2_atk_jll
 PATH = ""
 LIBPATH = ""
 LIBPATH_env = "PATH"
+LIBPATH_default = ""
 
 # Relative path to `libgailutil3`
 const libgailutil3_splitpath = ["bin", "libgailutil-3-0.dll"]
@@ -80,8 +81,6 @@ function __init__()
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
     # then append them to our own.
     foreach(p -> append!(PATH_list, p), (Glib_jll.PATH_list, Cairo_jll.PATH_list, Pango_jll.PATH_list, FriBidi_jll.PATH_list, FreeType2_jll.PATH_list, gdk_pixbuf_jll.PATH_list, Libepoxy_jll.PATH_list, ATK_jll.PATH_list, HarfBuzz_jll.PATH_list, xkbcommon_jll.PATH_list, iso_codes_jll.PATH_list, Wayland_jll.PATH_list, Xorg_libXrandr_jll.PATH_list, Xorg_libX11_jll.PATH_list, Xorg_libXrender_jll.PATH_list, Xorg_libXi_jll.PATH_list, Xorg_libXext_jll.PATH_list, Xorg_libXcursor_jll.PATH_list, Xorg_libXdamage_jll.PATH_list, Xorg_libXfixes_jll.PATH_list, Xorg_libXcomposite_jll.PATH_list, Xorg_libXinerama_jll.PATH_list, Fontconfig_jll.PATH_list, at_spi2_atk_jll.PATH_list,))
@@ -112,12 +111,8 @@ function __init__()
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ';')
-    global LIBPATH = join(LIBPATH_list, ';')
+    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
 
